d26ca5443b7ba236d5c225a76ae2bd87fea92894
Shadow Codez <ShadowCodez>
2021-01-24 21:22:25 +0530

    updated to uniquely identify keys and values of JSON

--- index.js
+++ index.js
@@ -116,46 +116,46 @@ let medicine_chart = {
   };
 
 
-  let test = {"Problems":[{"Cancer":["sample"]}]};
-  // console.log(test)
 
-  let finalDOM = diseaseDisplay(test);
-  
 
   function diseaseDisplay(a){
-  let input_data = Object.entries(a);
-  let i = 0;
-  var dd =input_data[0];
-    //itrate arrray
-    
-  console.log(dd[1]);
-  let output_data  = input_data.map(function(values,index){
-  let disease_names = ['Diabetes', 'Asthma', 'Cancer'];
-  let tags = ['medications','medicationsClasses','className','className2', 'labs'];
-  let associatedDrugs = ['associatedDrug','associatedDrug#2'];
-  let single_data = `
-    <div class="col-3">
-            <div class="card shadow">
-                <div class="card-body">
-                    <h3>Name: </h3>
-                </div>                    
-            </div>
-    </div>`;
-    return single_data;
+  let input_data = a;
+  let finalDOM = '';
 
-  })
-  console.log(output_data);
-  let finalDOM = `<div class="row" style="justify-content: center;" >${output_data.join("")}</div>`
-  document.getElementById("patient").innerHTML = finalDOM;
-  }
+  for (let j = 0; j < Object.values(medicine_chart.problems[0]).length; j++){
+
+  let output_data  = input_data.problems.map((data) => {
+                      return data.Diabetes.map(Diabetes => {
+                        return Diabetes.medications.map(medications => {
+                          return medications.medicationsClasses.map(medicationsClasses => {
+                            return medicationsClasses.className.map(className => {
+                              return className.associatedDrug.map(fuck =>{
+                                let single_data = `                
+                                  <div class="col-3">
+                                    <div class="card shadow">
+                                      <div class="card-body">
+                                        <h3>Name: ${Object.values(medicine_chart.problems[0]).length} </h3><br>
+                                      </div>                    
+                                    </div>
+                                  </div>`;
+                      return single_data; 
+                
+                                  })
+                                })
+                              })
+                            })
+
+                          })          
+                      })
+
+  finalDOM +=  output_data.join("")
+  document.getElementById("patient").innerHTML =`<div class="row">${finalDOM}<div>`;
+  }}
+
+  diseaseDisplay(medicine_chart);
+
   
-
-
-    
-
-
-
-
+  
 
 
 
